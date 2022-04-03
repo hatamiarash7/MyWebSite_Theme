bd5aa124b0532ffda906ecd435dbf1d8e797a8c7
hatamiarash7 <hatamiarash7@users.noreply.github.com>
2022-04-03 13:21:29 +0000

    [CI] Format Code!
    Format code using Prettier

--- assets/js/gallery-card.js
+++ assets/js/gallery-card.js
@@ -10,15 +10,15 @@
 
 (function (window, document) {
     var resizeImagesInGalleries = function resizeImagesInGalleries() {
-        var images = document.querySelectorAll('.kg-gallery-image img');
+        var images = document.querySelectorAll(".kg-gallery-image img");
         images.forEach(function (image) {
-            var container = image.closest('.kg-gallery-image');
+            var container = image.closest(".kg-gallery-image");
             var width = image.attributes.width.value;
             var height = image.attributes.height.value;
             var ratio = width / height;
-            container.style.flex = ratio + ' 1 0%';
+            container.style.flex = ratio + " 1 0%";
         });
     };
 
-    document.addEventListener('DOMContentLoaded', resizeImagesInGalleries);
+    document.addEventListener("DOMContentLoaded", resizeImagesInGalleries);
 })(window, document);
